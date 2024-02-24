ca3a0c1132df6cfd3bb318ac636a9e06f2fb8a7c
Md. Kaiser Ahmed <ahmedkaiser.001@gmail.com>
2024-02-24 22:36:52 +0600

    add utils app index file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+const port = process.env.PORT || 5000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error", error);
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is listening at port ${port}`);
+    });
+  })
+  .catch((error) => console.log("Database Connection Error", error));
